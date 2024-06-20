54e7586e8b0950c327ba9e5346d22960591d1b85
giovanniarigliani <gianni.arigliani@gmail.com>
2024-06-20 17:21:29 +0200

    aggiunta randomizzazione della lista

--- riddle.py
+++ riddle.py
@@ -1,4 +1,5 @@
 import re
+import random
 class Riddle:
     def __init__(self, question, level, right_answer, answers):
         self.question = question
@@ -9,3 +10,30 @@ class Riddle:
     def check_answer(self, answer):
         return answer == self.right_answer
 
+    def get_string(self):
+        return f"domanda {self.question} -- livello {self.level} -- corretta {self.right_answer} - lista: {self.answers}"
+
+
+def get_text(file_path):
+        with open(file_path, 'r') as c:
+            file_text = c.read()
+        riddle_list = []
+
+        for rid in re.split('\n\n', file_text):
+            riddle_elements = re.split('\n', rid)
+            if len(riddle_elements) == 6:
+                question = riddle_elements[0]
+                level = riddle_elements[1]
+                right_answer = riddle_elements[2]
+                answers = [riddle_elements[2], riddle_elements[3], riddle_elements[4], riddle_elements[5]]
+                new_riddle = Riddle(question, level, right_answer, answers)
+                riddle_list.append(new_riddle)
+
+            else:
+                print("error: the text format is not correct")
+        return riddle_list
+
+def shuffle_raddle(txt):
+    for i in txt:
+        random.shuffle(i.answers)
+    return txt
